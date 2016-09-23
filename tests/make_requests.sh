e8e247f90d449b049a91148707f438e9a5e24846
Shawn Lower <shawn@shawnlower.com>
2016-09-24 01:34:16 +0700

    Adding more curl tests, add comments

--- tests/make_requests.sh
+++ tests/make_requests.sh
@@ -19,19 +19,25 @@ function do_test(){
     echo "Running test: $command"
     echo "======================"
 
-    eval $command
+    rv=$(eval $command)
     rc=$?
     echo -e "\n\n"
 
     if [[ $rc -ne 0 ]]; then
         echo "WARNING: Command returned non-zero value: $rc" >&2
+        echo "Output from command (in {{{ }}})\n{{{$rv}}}"
         return 1
     fi
 }
 
+# Basic get test
 do_test curl -v $URL
 
+# Get tests with 'Accept' header set
 for header in 'text/html' 'application/json'; do
     do_test curl -v -H \"Accept: ${header}\" $URL
 done
 
+# POST method testing
+do_test curl -v -X POST '{"foo": "This is a foo test from cURL"}'
+do_test curl -v -X POST '{"bar": "This is a bar test from cURL"}'
