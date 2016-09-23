f5f6a7472b0fe318108dc256891a7767ea8b90e4
Shawn Lower <shawn@shawnlower.com>
2016-09-23 22:50:05 +0700

    Return customized message based on 'Accept' header

--- flask_exercise.py
+++ flask_exercise.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    # Inspect the list of the accepted types returned by the client, as per RFC2616.
+    accept = request.headers.get('Accept', "").split(',')
 
+    # Return our hello message as either text or json, depending on the value
+    # NOTE: it's possible for the client to pass both values, in which case we
+    # default to the first case
+    if 'text/html' in accept:
+        return "<p>Hello, World</p>"
+    elif 'application/json' in accept:
+        return '{"message": "Good morning"}'
 
 if __name__ == '__main__':
     app.run()
