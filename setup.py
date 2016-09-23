c2e69e65bac170356e109f8ff12ac0e255a6bf17
Shawn Lower <shawn@shawnlower.com>
2016-09-24 05:34:45 +0700

    Adding setuptools stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='flask_exercise',
+      version='0.1',
+      description='Flask exercise homework',
+      url='http://github.com/shawnlower/flask_exercise',
+      author='Shawn Lower',
+      author_email='shawn@shawnlower.com',
+      license='MIT',
+      packages=['flask_exercise'],
+      zip_safe=False)
+
